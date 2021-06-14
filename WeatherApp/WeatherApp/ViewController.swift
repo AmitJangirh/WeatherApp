393e539e8c4cfc031110ad60c255748eb8e865d1
Amit Jangirh <amitjangirh@gmail.com>
2021-06-14 11:43:22 +0530

    Added empty app project

---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Amit Jangirh on 14/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
